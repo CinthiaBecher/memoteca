413bfa72e3d2427d11b8976e49c301f514fdbb37
CinthiaBecher <cinthia_becher@hotmail.com>
2023-01-22 11:06:37 -0300

    first commit

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,26 +1,38 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PensamentoService } from './../pensamento.service';
+import { Pensamento } from './../pensamento';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
   styleUrls: ['./criar-pensamento.component.css']
 })
-export class CriarPensamentoComponent {
+export class CriarPensamentoComponent implements OnInit {
 
-  pensamento = {
-      id: '1',
-      conteudo: 'Aprendendo Angular',
-      autoria: 'Dev', 
-      modelo: 'modelo1'
+  pensamento: Pensamento = {
+
+    conteudo: '',
+    autoria: '',
+    modelo: 'modelo1'
   }
 
-  criarPensamento(){
-    alert("Novo Pensamento Criado");
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
   }
 
-  cancelar(){
-    alert("Cancelado");
+  criarPensamento() {
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    })
   }
+
+  cancelar() {
+    this.router.navigate(['/listarPensamento'])
+  }
+
 }
-
-
